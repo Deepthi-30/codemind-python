ee520f95409cc28d94c3f612bb871c54c5935b74
Deepthi-30 <84894277+Deepthi-30@users.noreply.github.com>
2022-10-09 12:59:58 +0530

    a=int(input()) b=int(input()) sum1=0 sum2=0 for i in range(1,a+1):     if a%i==0:         sum1+=i for i in range(1,b+1):     if b%i==0:         sum2+=i if sum1==sum2:     print("Amicable") else:     print("Not Amicable")

--- Amicable_Numbers.py
+++ Amicable_Numbers.py
@@ -1,14 +1,14 @@
 a=int(input())
 b=int(input())
-sum=0
-for i in range(1,a):
-    if a%i==0:
-        sum+=i
 sum1=0
-for i in range(1,b):
-    if b%i==0:
+sum2=0
+for i in range(1,a+1):
+    if a%i==0:
         sum1+=i
-if sum==b and sum1==a:
+for i in range(1,b+1):
+    if b%i==0:
+        sum2+=i
+if sum1==sum2:
     print("Amicable")
 else:
     print("Not Amicable")
