109e5e8d41f80f789942427b3cd04321eb3d6954
Deepthi-30 <84894277+Deepthi-30@users.noreply.github.com>
2022-10-09 13:02:22 +0530

    a=int(input()) a=str(a) a=list(a) b=[] for i in a:     if a.count(i)<=1:         b.append(i) if len(b)==len(a):     print("Unique Number") else:     print("Not Unique Number")

---
+++ Unique_number.py
@@ -0,0 +1,11 @@
+a=int(input())
+a=str(a)
+a=list(a)
+b=[]
+for i in a:
+    if a.count(i)<=1:
+        b.append(i)
+if len(b)==len(a):
+    print("Unique Number")
+else:
+    print("Not Unique Number")
